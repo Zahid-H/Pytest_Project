63fe7b00c9362b05364174d4ff4bc0dead2ea50d
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2025-03-09 12:25:18 +0600

    made changes in the test_admin_login.py file code

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,8 +1,27 @@
 import pytest
 from selenium import webdriver
-
+# for defining browser commandline input
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome", help="Specify the browser: chrome or firefox or edge")
+#Created two fixture setup and browser fixture
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome()
+#receiving command line input from browser
+def browser(request):
+    return request.config.getoption("--browser")
+# Value received from the
+@pytest.fixture()
+def setup(browser):
+    #diver instance variable
+    global driver
+    if browser == "chrome":
+        return webdriver.Chrome()
+    elif browser == "firefox":
+        return webdriver.Firefox()
+    elif browser == "edge":
+        return webdriver.Edge()
+    else:
+        raise Exception("Browser not supported")
+
     return driver
+
 
